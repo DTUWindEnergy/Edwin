788015e5148007e2e9efbaba774834f43faad91f
mikf <mikf@dtu.dk>
2021-10-12 11:35:18 +0200

    pep8

--- edwin/tests/test_wind_farm_network.py
+++ edwin/tests/test_wind_farm_network.py
@@ -1,8 +1,9 @@
 from edwin.tests import npt
 from edwin.wind_farm_network import WindFarmNetwork
 
-method='test'
+method = 'test'
 wfn = WindFarmNetwork(method)
+
 
 def test_wind_farm_network():
     assert wfn.method == 'test'
