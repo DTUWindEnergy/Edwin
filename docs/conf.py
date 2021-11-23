25a0811204d2424a135535f8172b3682e7104e08
Mikkel Friis-Møller <mikf@dtu.dk>
2021-11-23 12:26:04 +0000

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -137,8 +137,8 @@ nbsphinx_prolog = r"""
     .. nbinfo::
 
 
-        :raw-html:`<a href="https://colab.research.google.com/github/DTUWindEnergy/EDWIN/blob/master/{{ docname }}"><img alt="Open and run in Colab (interactive)" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>
-        <a href="https://gitlab.windenergy.dtu.dk/TOPFARM/EDWIN/-/tree/master/{{ docname }}"><img alt="Edit on Gitlab" src="https://img.shields.io/badge/Edit%20on-Gitlab-blue?style=flat&logo=gitlab" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<a href="https://colab.research.google.com/github/DTUWindEnergy/EDWIN/blob/main/{{ docname }}"><img alt="Open and run in Colab (interactive)" src="https://colab.research.google.com/assets/colab-badge.svg" style="vertical-align:text-bottom"></a>
+        <a href="https://gitlab.windenergy.dtu.dk/TOPFARM/EDWIN/-/tree/main/{{ docname }}"><img alt="Edit on Gitlab" src="https://img.shields.io/badge/Edit%20on-Gitlab-blue?style=flat&logo=gitlab" style="vertical-align:text-bottom"></a>`
 
 """
 
